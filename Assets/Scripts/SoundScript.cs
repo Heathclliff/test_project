9736677e2ed182b82bd4d580935eaf5d260a961b
Heathclliff <potapchik97@mail.ru>
2017-09-20 20:35:05 +0300

    added first scene

---
+++ Assets/Scripts/SoundScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour {
+
+    public GameObject soundOn, soundOff;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetString("Music") != "no")
+        {
+            soundOn.SetActive(true);
+            soundOff.SetActive(false);
+        }
+        else
+        {
+            soundOn.SetActive(false);
+            soundOff.SetActive(true);
+        }
+    }
+
+    void OnMouseUpAsButton()
+    {
+        if(PlayerPrefs.GetString("Music") != "no")
+        {
+            GameObject.Find("AudioClick").GetComponent<AudioSource>().Play();
+        }
+
+        if (PlayerPrefs.GetString("Music") != "no")
+        {
+            PlayerPrefs.SetString("Music", "no");
+            soundOn.SetActive(false);
+            soundOff.SetActive(true);
+        }
+        else
+        {
+            PlayerPrefs.SetString("Music", "yes");
+            soundOn.SetActive(true);
+            soundOff.SetActive(false);
+        }
+    }
+}
